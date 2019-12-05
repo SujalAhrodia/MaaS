e46206876ee6c910f0b9cda8ca12130b828eb51f
t11_vm4 <arangar2@ncsu.edu>
2019-12-05 01:38:58 +0000

    Logic Layer for VPC architecture & south bound for interface creation

--- Milestone2/create_interface.py
+++ Milestone2/create_interface.py
@@ -31,12 +31,6 @@ def execute_command(ssh_client, command):
             while stdout.channel.recv_ready():
                 alldata+=stdout.channel.recv(1024)
             #print(str(alldata, 'utf8'))
-    #print(type(command))
-    #print(command)
-    #print(stderr.read())
-    #output = ''
-    #for line in stdout:
-    #     output += line
     if isinstance(alldata,str):
          return alldata
     return alldata.decode('utf-8')
@@ -46,30 +40,31 @@ def close_ssh_conn(ssh_client):
 
 
 def attach_interface(vm_id,tenant_id,subnet,ip,zone_id,route_flag):
-    zone_ssh=create_ssh_conn(zones[zone_id][0],'ece792',zones[zone_id][1])
-    print(vm_id,zones[zone_id][0])
-    cmd="sudo virsh domifaddr "+vm_id+"|awk 'FNR==3 {print $4}'"
-    print(cmd)
-    addr=execute_command(zone_ssh,cmd).split('/')[0]
-    print(addr)
-    mac=execute_command(zone_ssh,'sudo virsh domiflist '+vm_id+' | grep '+vm_id+subnet+' | awk \'{print $5}\'')
-    close_ssh_conn(zone_ssh)
-    vm_ssh=create_ssh_conn(addr,'root','rootroot')
-    dev_name = execute_command(vm_ssh, "ip -o  link | grep "+mac)
-    #+"|awk '{ print $2 }')
-    print(mac,dev_name)
-    dev_name=dev_name.split()[1][:-1]
-    print(dev_name)
-    execute_command(vm_ssh,"ip addr add "+ip+"/25 dev "+dev_name)
-    execute_command(vm_ssh,"ip link set dev "+dev_name+" up")
-    if route_flag:
-        gw_ip = '.'.join(ip.split('.')[:-1]) + '.1'
-        execute_command(vm_ssh, "ip route del default")
-        execute_command(vm_ssh, "ip route add default dev " + dev_name + "via " + gw_ip)
-    close_ssh_conn(vm_ssh)
-    return addr
+    try:
+        zone_ssh=create_ssh_conn(zones[zone_id][0],'ece792',zones[zone_id][1])
+        print(vm_id,zones[zone_id][0])
+        cmd="sudo virsh domifaddr "+vm_id+"|awk 'FNR==3 {print $4}'"
+        print(cmd)
+        addr=execute_command(zone_ssh,cmd).split('/')[0]
+        print(addr)
+        #execute_command(zone_ssh,'virsh attach-interface --domain {0} --model virtio --type network --config --live --source {1} --target {2}'.format(vm_id,'net_'+tenant_id+subnet,vm_id+subnet))
+        mac=execute_command(zone_ssh,'sudo virsh domiflist '+vm_id+' | grep '+vm_id+subnet+' | awk \'{print $5}\'')
+        close_ssh_conn(zone_ssh)
+        vm_ssh=create_ssh_conn(addr,'root','rootroot')
+        dev_name = execute_command(vm_ssh, "ip -o  link | grep "+mac)
+        print(mac,dev_name)
+        dev_name=dev_name.split()[1][:-1]
+        print(dev_name)
+        execute_command(vm_ssh,"ip addr add "+ip+"/25 dev "+dev_name)
+        execute_command(vm_ssh,"ip link set dev "+dev_name+" up")
+        if route_flag:
+            gw_ip = '.'.join(ip.split('.')[:-1]) + '.1'
+            execute_command(vm_ssh, "ip route del default")
+            execute_command(vm_ssh, "ip route add default dev " + dev_name + "via " + gw_ip)
+        close_ssh_conn(vm_ssh)
+        return addr
+    except Exception:
+        return -1
     
 if __name__=='__main__':
     attach_interface('q3VM1','q3br_','net','11.11.91.12','zone2')
-    #except Exception as e:
-     #   print(e)
