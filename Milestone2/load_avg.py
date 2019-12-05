c5615d5869f5b941cd1a1deb3421ae91ed9d956d
Sujal Ahrodia <ssujal@ncsu.edu>
2019-12-05 18:56:29 -0500

    typo

--- Milestone2/load_avg.py
+++ Milestone2/load_avg.py
@@ -1,34 +1,32 @@
+#!/bin/python
+
 #LOGGING THE LOAD AVERAGES
-# uptime | awk '{print $10 $11 $12}'
 
-# a=$(uptime | awk '{print substr($10,0,4)}')
-# b=$(uptime | awk '{print substr($11,0,4)}')
-# echo $(uptime)
-
-# done
-
-#!/usr/bin/python
 import collectd
 import subprocess 
 
 def configer(confObj):
     collectd.info('config called')
 
-
 def init_fun():
     collectd.info('my py module init called')
 
-
 def reader():
     collectd.info('reader called')
+    cmd = "uptime | awk '{print $9 $10 $11}'"
+    # p2 = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
+    collectd.info('poen called')
 
-    cmd = "uptime | awk '{print $10 $11 $12}'"
-    p2 = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
-    (out, err) = p2.communicate()
-    a,b,c = out.decode("utf-8").split('\n')[0].split(',')
-    a = float(a)
-    b = float(b)
-    c = float(c)
+    # (out, err) = p2.communicate()
+    # a,b,c = out.decode("utf-8").split('\n')[0].split(',')
+    # a = float(a)
+    # b = float(b)
+    # c = float(c)
+    a=0.01
+    b=0.01
+    c=0.01
+    collectd.info('decoded ')
+
     # Dispatch value to collectd
     val = collectd.Values(type='load')
     val.plugin = 'load_avg'
