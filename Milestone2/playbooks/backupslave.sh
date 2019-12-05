49a8c3462de482844e110d945d5069de72a95558
Sujal Ahrodia <sujalahrodia@gmail.com>
2019-12-05 16:16:54 -0500

    idfb play plus testing scripts

--- Milestone2/playbooks/backupslave.sh
+++ Milestone2/playbooks/backupslave.sh
@@ -8,7 +8,7 @@ echo "I am not the master"
 else
 influxd backup -portable -database collectd ~/infdb_bak
 echo "I am the master"
-scp -r ~/infdb_bak root@192.168.123.80:~/
+scp -o StrictHostKeyChecking=no -r ~/infdb_bak root@192.168.123.80:~/
 ssh -o StrictHostKeyChecking=no root@192.168.123.80  'curl -i -XPOST http://localhost:8086/query --data-urlencode "q=DROP DATABASE collectd" | influxd restore -portable ~/infdb_bak'
 sleep 5
 echo Removing backup at slave
